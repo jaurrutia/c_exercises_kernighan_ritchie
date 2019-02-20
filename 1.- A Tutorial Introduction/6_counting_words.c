7f509f56bd56cd8492ba1c8fd8b1d0b2bd84f80b
jaurrutia <jaurrutia.95@gmail.com>
2019-02-19 23:43:56 -0600

    some more advances

--- 1.- A Tutorial Introduction/6_counting_words.c
+++ 1.- A Tutorial Introduction/6_counting_words.c
@@ -1,7 +1,9 @@
 /*Jonathan Urrutia 2019/02/11
 Sixth example. Word counting.
-We'll define a word as any sequence without a \t, ' ' or \n.
-Somehow is not working :c It caunts all word's chracater a a word....
+We'll define a word as any sequence without a \t, ' ' or \n. and will learn how
+the if statement works, as well as the constants
+
+Excercises 1-11,12
 */
 
 #include <stdio.h>
@@ -15,22 +17,30 @@ main()
   int c, nc, nw, nl, state;  /* charcater, number of charcaters, - words, -lines */
 
   nl = 1;
-  nw = 0;
-  nc = 0;
+  nw = nc = 0;
 
   state = OUT;
 
-  printf("We count characters, words and lines. Write something:\n");
+  printf("We count characters, words and lines but before, we list all of your words separatelly. Write something:\n");
   while((c = getchar()) != EOF){
     ++nc;
-    if (c == '\n')
+    if(c == '\n')
       ++nl;
-    if ( c == ' ' || c == '\n' || c == '\t')
+    if (c == ' ' || c == '\n' || c == '\t'){
       state = OUT;
-    else if( state == OUT){
-      state == IN;
+      printf("\n");
+    }else if(state == OUT){
+      state = IN;
+      putchar(c);
       ++nw;
+    }else if(state == IN){
+      putchar(c);
     }
   }
   printf("\n There are %d characters, %d lines and %d words.\n", nc, nl, nw);
+
+
+
+
+
 }
