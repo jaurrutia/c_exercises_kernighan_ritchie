338854197db40fe36890f012cdb8c3298dbe9ffe
jaurrutia <jaurrutia.95@gmail.com>
2020-10-26 10:53:24 -0600

    Introduction done. The external variables excercises where ommited.

--- 1.- A Tutorial Introduction/9_functions_character_of_arrays.c
+++ 1.- A Tutorial Introduction/9_functions_character_of_arrays.c
@@ -1,6 +1,6 @@
 /*Jonathan Urrutia 2020/10/21
-Ninth example.
-Excercises 1-15
+Ninth example. Functions withs arrays: This are not called by value, so every effect on the function it it tracked y the array.
+Excercises 1-16,17
 */
 
 
@@ -8,6 +8,7 @@ Excercises 1-15
 #define MAXLINE 1000                       /*Maximum input line size*/
 
 int get_line( char line[], int maxline);    // I had to change getline (as in the book) to get_line due to declaration problemns related to the stdio.h library
+int get_line_arbitrary( char line[], int maxline);
 void copy(char to[], char from[]);
 
 /*Print the longest input line*/
@@ -17,13 +18,17 @@ int main()
   char line[MAXLINE], longest[MAXLINE];   /*Current input line and  longest line input*/
 
   max = 0;
-  while((len = get_line(line,MAXLINE)) > 0)
+  while((len = get_line(line,MAXLINE)) > 0){
+    if(len >= 80){
+      printf("This line has more than 80 characters:\n %s\n", line );
+    }
     if (len > max){
       max = len;
       copy( longest, line);
     }
+  }
   if (max > 0 )                            /*lines have at least one char so max > 0 is equivalent to 'There is a line'*/
-    printf("The longest line has %d characters and it is:\n %s\n", max, longest );
+    printf("\n\n\n The longest line has %d characters and it is:\n %s\n", max, longest );
 
 return 0;
 
@@ -54,7 +59,7 @@ int get_line_arbitrary( char s[], int lim)
 {
   int c, i, j;
 
-  j = 0
+  j = 0;
   for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i) // i -> # in the array that is either EOF or \n
     if (  i < lim - 2){
       s[j] = c;
